3cf251ed991d824442044456353741ecd5c8f7cb
Michael <michael.danielnaguib@studenti.unimi.it>
2016-06-11 20:14:36 +0200

    completato Random : 4 pomodori + 1/2

--- src/main/java/it/unimi/di/sweng/lasergame/RandomCells.java
+++ src/main/java/it/unimi/di/sweng/lasergame/RandomCells.java
@@ -9,9 +9,11 @@ public class RandomCells {
 	public  CellStrategy[][]  filler(CellStrategy[][] board){//passo la matrice delle celle che so essere 5x5
 		Random rand=new Random();
 		boolean setTarget=false;
+		
 		this.nMirrors=rand.nextInt(13)+1;// cosi il numero di specchi sarà >=1 && <=13
 		 
 		int r,c;
+		
 		for(int i=0;i<this.nMirrors;i++){//ciclo finche ho specchi da mettere
 				r=rand.nextInt(5);
 				c=rand.nextInt(5);
@@ -20,18 +22,20 @@ public class RandomCells {
 				}
 			
 		}
-		r=0; c=0;//reinizializzo gli indici della matrice
+		
 		
 		while(!setTarget){// aggiungo il target 
-			if((r=rand.nextInt(5))!=4 &&(c=rand.nextInt(5))!=0){
+			if((r=rand.nextInt(5))!=4 &&(c=rand.nextInt(5))!=0){//controllo che la posizione scelta per il target sia diversa da <4,0>(cella in basso a sx)
 				board[r][c]=new TargetCell();
 				setTarget=true;
 			}	
 		}
 		
-		
-			
-		
+		for(r=0;r<5;r++)
+			for(c=0;c<5;c++){
+				if (board[r][c]==null)
+					board[r][c]=new EmptyCell();
+			}
 		
 	}
 }

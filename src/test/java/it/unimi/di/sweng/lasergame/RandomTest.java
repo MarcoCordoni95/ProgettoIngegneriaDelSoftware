bc6254c662f51b4670a5de9ac319539726d51b7c
Michael <michael.danielnaguib@studenti.unimi.it>
2016-06-12 11:01:17 +0200

    completato test random

--- src/test/java/it/unimi/di/sweng/lasergame/RandomTest.java
+++ src/test/java/it/unimi/di/sweng/lasergame/RandomTest.java
@@ -31,7 +31,14 @@ public class RandomTest {
 		
 		
         
+       board=rand.filler(board);
        
+       for(int r=0;r<5;r++)
+    	   for(int c=0;c<5;c++){
+    		   
+    		   assertThat(board[r][c]).isNotEqualTo(null);
+    		   
+    	   }
 		
 		
 		

bc7efbc265dc341d016683d4aa25bfde63c79038
Marcoluzz <marco95cordo@hotmail.it>
2016-06-13 20:27:57 +0200

    Aggiunti test su button

---
+++ src/test/java/it/unimi/di/sweng/lasergame/TestButton.java
@@ -0,0 +1,38 @@
+package it.unimi.di.sweng.lasergame;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import GraphicView.EmptyButton;
+import GraphicView.MirrorButton;
+import GraphicView.TargetButton;
+
+public class TestButton {
+	
+	@Test
+	public void TestEmptyButton(){
+		EmptyButton eb = new EmptyButton(1);
+		assertEquals(eb.getNumb(),1);
+		assertEquals(eb.getWhoamI(),"");
+	}
+	
+	@Test
+	public void TestMirrorButton(){
+		MirrorButton mb = new MirrorButton(2,"/");
+		assertEquals(mb.getNumb(),2);
+		assertEquals(mb.getWhoamI(),"/");
+		mb=mb.change();
+		assertEquals(mb.getWhoamI(),"\\");
+		mb=mb.change();
+		assertEquals(mb.getWhoamI(),"/");
+	}
+	
+	@Test
+	public void TestTargetButton(){
+		TargetButton tb = new TargetButton(3,"t");
+		assertEquals(tb.getNumb(),3);
+		assertEquals(tb.getWhoamI(),"t");
+	}
+
+}

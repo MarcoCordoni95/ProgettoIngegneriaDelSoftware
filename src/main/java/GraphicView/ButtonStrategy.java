6f149e49a8e69eecd5012df1e48ada64afe821fd
Michael <michael.danielnaguib@studenti.unimi.it>
2016-06-13 11:13:09 +0200

    altre piccole modifiche al button strategy

--- src/main/java/GraphicView/ButtonStrategy.java
+++ src/main/java/GraphicView/ButtonStrategy.java
@@ -4,10 +4,18 @@ import javax.swing.JButton;
 
 public abstract class ButtonStrategy extends JButton{
 	private String whoamI;
+	private int numb;
 	public ButtonStrategy(int i,String c){
 		super(c+"");
+		this.numb=i;
 		this.whoamI=c;
 		this.setName(""+i);
+	}
+	public String getWhoamI(){
+		return this.whoamI;
+	}
+	public int getNumb(){
+		return this.numb;
 	}
 	
 	
--- src/main/java/GraphicView/MirrorButton.java
+++ src/main/java/GraphicView/MirrorButton.java
@@ -3,9 +3,19 @@ package GraphicView;
 
 
 public class MirrorButton extends ButtonStrategy {
-
+	
 	public MirrorButton(int i,String c){//il numero passato sarà un identificatore univoco all'interno della griglia
 		super(i,c);
 		
-		}
+	}
+	
+	public MirrorButton change() {
+		if(this.getWhoamI().equals("/"))
+			return  new MirrorButton(this.getNumb(),"\\");
+		if(this.getWhoamI().equals("\\"))
+			return  new MirrorButton(this.getNumb(),"/");
+		return null;
+		
+		
+	}
 }
